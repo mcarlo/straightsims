906471440210e368fa71b5b12683a13dd623fabe
mcarlo <tom.anichini@gmail.com>
2015-09-03 08:09:36 -0700

    Added some counts at bottom

--- server.R
+++ server.R
@@ -1,40 +1,44 @@
 rm(list = ls())
 library(shiny); library(scales)
-setwd("D:/Documents/GitHub/wtpP")
+setwd("~/GitHub/straightsims")
 load("app2015wk01.RData")
 
 # Define a server for the Shiny app
 shinyServer(function(input, output) { # input <- data.frame(players = 20)
   sizeIndex <- reactive({input$players/5 * 2})
-  
+
   results <- reactive({playersBest[[sizeIndex()]]})
-  
+
   outrightPicks <- reactive({results()$outPicks})
-  
+
   outright <- reactive({
     vec <- weekFile$Victor
     vec[outrightPicks() == 0] <- weekFile$Underdog[outrightPicks() == 0]
     vec
   })
-  
+
   output$outrightWins <- reactive({results()$outW})
-                                  
+
+  output$avgOut <- reactive({results()$avgOut})
+
+  output$avgMost <- reactive({results()$avgMost})
+
   output$outrightPoints <- reactive({results()$outPoints})
 
   mostPicks <- reactive({results()$mostPicks})
-  
+
   mostWins <- reactive({
     vec <- weekFile$Victor
     vec[mostPicks() == 0] <- weekFile$Underdog[mostPicks() == 0]
     vec
   })
-  
+
   output$mostWins <- reactive({results()$mostW})
-  
+
   output$mostPoints <- reactive({results()$mostPoints})
-  
+
   output$outright <- renderTable({cbind(Game = weekFile$Game[order(weekFile$order)], Victor = outright()[order(weekFile$order)])}, include.rownames=F)
   output$most <- renderTable({cbind(Game = weekFile$Game[order(weekFile$order)], Victor = mostWins()[order(weekFile$order)])}, include.rownames=F)
-  
+
   output$week <- renderTable({data.frame(Game = weekFile$Game, Victor = weekFile$Victor)})
 })
--- ui.R
+++ ui.R
@@ -3,18 +3,18 @@ library(shiny)
 # Define the overall UI
 shinyUI(fluidPage(
   fluidRow(
-     column(4, 
+     column(4,
            h1("WinThatPool")
            )
 #      ,
-#      column(5, 
+#      column(5,
 #             p("Tactical picks for 2015, Week 1 (Straight picks, no confidence points)")
 #      )
     ),
     fluidRow(
       column(4, p(""))
 #       ,
-#       column(4, 
+#       column(4,
 #              sliderInput("players", "   Number of Players in Pool:", min = 5,
 #                          max = 100, step = 5, value = 25))
       ),
@@ -23,7 +23,7 @@ shinyUI(fluidPage(
     fluidRow(
       column(4,
              h4("Most simulated outright wins (no ties)")),
-      column(4, 
+      column(4,
              p("Tactical picks for 2015, Week 1"),
 p("Straight picks, no confidence points)")
       ),
@@ -33,12 +33,12 @@ p("Straight picks, no confidence points)")
     fluidRow(
       column(4,
              tableOutput(outputId="outright")),
-      column(4, 
+      column(4,
              sliderInput("players", "   Number of Players in Pool:", min = 5,
                          max = 100, step = 5, value = 25)),
       column(4,
              tableOutput(outputId="most"))
-      
+
     ),
   fluidRow(
     column(3, p("")),
@@ -47,9 +47,17 @@ p("Straight picks, no confidence points)")
            p("Notice that as you play with the slider controlling the number of other players in your pool, the higher the number of players, the greater the number of upsets in the winningest picks. Of course the number of players in your pool doesn't affect the outcome of games, but it does affect your chance of amassing the most points."),
            p(""),
            p("WinThatPool simulated pool results equivalent to 100 complete NFL seasons using this week's win probabilities for each game and this week's model of other pool entries.
-             The first set listed above won the most times. The second set listed above won or tied the most times.")
+             The first set listed above won the most times. The second set listed above won or tied the most times."),
+           h6("Outright Wins " ,textOutput("outrightWins")),
+
+           h6("Average Wins " ,textOutput("avgOut")),
+
+           h6("Wins or ties " ,textOutput("mostWins")),
+
+           h6("Average Wins or ties " ,textOutput("avgMost"))
+
     )
-    
+
   )
     )
 )
